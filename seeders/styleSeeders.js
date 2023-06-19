bb875edea0c8f9050fc5a984a3d3a4745c5816c3
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-19 15:28:48 -0300

    alcohol by volume (abv) to every product

--- seeders/styleSeeders.js
+++ seeders/styleSeeders.js
@@ -7,42 +7,50 @@ module.exports = async () => {
       price: 10.99,
       description:
         " A unique and flavorful amber or brown beer with a malty character and caramel notes. Balancing sweetness and bitterness, it offers a medium to full body with a prominent malt profile and subtle hints of toffee and nuts. Perfect for those seeking intense and full-bodied beer experiences.",
+      abv: 5.6,
     },
     {
       name: "IPA",
       price: 10.99,
       description:
         "An exceptional India Pale Ale with a golden color and captivating aroma of citrus and tropical fruits. It perfectly balances hop bitterness and malt sweetness, offering a refreshing and invigorating flavor with notes of grapefruit, pine, and resin. A bold and vibrant beer journey.",
+      abv: 4.5,
     },
     {
       name: "Stout",
       price: 10.99,
       description:
         "Cerveza negra, con buena formación de espuma. Presenta aromas y sabores de malta tostada a café y cítricos a lúpulo americano. El final es seco y de amargor creciente.",
+       abv: 5.5,
     },
     {
       name: "Blonde",
       price: 10.99,
       description:
         "Cerveza rubia clara, refrescante y altamente bebible. La mezcla de levaduras Ale y Lager hacen que esta cerveza sea muy ligera y agradable en el paladar.",
+      abv: 5.2,
     },
     {
       name: "APA",
       price: 10.99,
       description:
         "Cerveza dorada y clara, que presenta notas frutales complejas en el aroma y sabor. De cuerpo mediano, posee buena cremosidad. El alcohol brinda un agradable calentamiento en la garganta.",
+      abv: 6
     },
     {
       name: "ZERO",
       price: 10.99,
       description:
         "Cerveza dorada y clara, que presenta notas frutales complejas en el aroma y sabor. De cuerpo mediano, posee buena cremosidad. El alcohol brinda un agradable calentamiento en la garganta.",
+      abv: 0
+      
     },
     {
       name: "Pilsener",
       price: 10.99,
       description:
         "Cerveza dorada y clara, que presenta notas frutales complejas en el aroma y sabor. De cuerpo mediano, posee buena cremosidad. El alcohol brinda un agradable calentamiento en la garganta.",
+      abv: 5
     }
   ];
 

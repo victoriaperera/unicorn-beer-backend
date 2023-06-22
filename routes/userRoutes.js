ea9a81f700bd594a83ce8902e15a640a8b4a025e
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-22 16:12:34 -0300

    admin token userDelete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,10 @@ router.get("/", userController.index);
 router.get("/", userController.show);
 router.post("/", userController.store);
 router.patch("/:id", userController.update);
-router.delete("/", userController.destroy);
+router.delete(
+  "/",
+  checkJwt({ secret: process.env.JWT_ADMIN_SECRET_KEY, algorithms: ["HS256"] }),
+  userController.destroy,
+);
 
 module.exports = router;

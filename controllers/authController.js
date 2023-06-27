c6c1230788c2dbfa1467243e73936f6813b68575
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-27 11:56:28 -0300

    user orders in login

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,8 +14,10 @@ async function login(req, res) {
       });
       delete user._doc.password;
       user._doc.token = token;
-      const orders = await Order.find({ where: { user: user.id } });
+
+      const orders = await Order.find({ user: user._id });
       user._doc.orders = orders;
+      console.log(orders);
 
       return res.status(201).json(user);
     }

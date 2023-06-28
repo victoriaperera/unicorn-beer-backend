3b7c00565c7bfbaafa51bb60373128a7cd5a478d
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-28 15:19:54 -0300

    Se borran Admines

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,7 +6,7 @@ var { expressjwt: checkJwt } = require("express-jwt");
 router.get("/", adminController.index);
 router.post("/token", adminController.login);
 router.delete(
-  "/:id",
+  "/",
   checkJwt({ secret: process.env.JWT_ADMIN_SECRET_KEY, algorithms: ["HS256"] }),
   adminController.destroy,
 );

7eee6d7ddc4eeda37cfb821f3036498f40460481
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-29 14:15:42 -0300

    send order mail minor fix

--- middlewares/sendMailOrders.js
+++ middlewares/sendMailOrders.js
@@ -7,7 +7,9 @@ const sendEmail = (order, updated) => {
     to: [
       "nicomar2004@gmail.com",
       "mvperera@gmail.com",
-      "lorec.rincon@gmail.com", "estebanc_b@hotmail.com", "noahsugliano@gmail.com",
+      "lorec.rincon@gmail.com",
+      "estebanc_b@hotmail.com",
+      "noahsugliano@gmail.com",
     ],
     from: "unicorn.craftbeer@gmail.com",
     subject: `There is ${updated ? "a new order dispatched" : "an update"} on order N°${order.id}`,
@@ -15,7 +17,7 @@ const sendEmail = (order, updated) => {
     html: `<h1>Hello Admins,</h1>
     <p>There is a new order dispatched on ${order.createdAt}!</p>
     <p>To view more information, click on the following link:</p>
-    <p><a href="http://localhost:5173/admins/${order.id}">[link to order]</a></p>`,
+    <p><a href="http://localhost:5173/admin">[link to order]</a></p>`,
   };
 
   sgMail.send(msg).then(() => console.log("Email sent"));

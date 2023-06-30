fe58073f1133693e90c90d78127df03cccfeb899
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-30 02:37:50 -0300

    store product fix

--- controllers/productController.js
+++ controllers/productController.js
@@ -17,7 +17,7 @@ async function show(req, res) {
 async function store(req, res) {
   console.log(req.body);
   try {
-    const container = await Container.findById(req.body.container);
+    const container = await Container.find({ name: req.body.container });
     const style = await Style.findById(req.body.style);
     let name = ``;
     if (container.name === "keg") {

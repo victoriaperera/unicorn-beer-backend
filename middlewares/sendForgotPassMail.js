efd6783322993e5d005d5af319867d35cfab8983
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-30 16:34:47 -0300

    sendMail minor fix

--- middlewares/sendForgotPassMail.js
+++ middlewares/sendForgotPassMail.js
@@ -11,7 +11,7 @@ const sendForgotPassMail = (user) => {
     html: `<h1>Hello ${user.firstname},</h1>
         <p>We have received a password change request for your Unicorn beer account (${user.email}).</p>
         <p>If you did not ask to change your password, then you can ignore this email and your password will not be changed.</p>
-        <p><a href="http://localhost:5173/forgotpassword/${user.id}">[Change you password]</a></p>`,
+        <p><a href="http://unicornbeer.vercel.app/forgotpassword/${user.id}">[Change you password]</a></p>`,
   };
 
   sgMail.send(msg).then(() => console.log("Email sent"));

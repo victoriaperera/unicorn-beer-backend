c219268614ad9314ab3713ea9f23c701eb188279
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-30 03:30:54 -0300

    product delete fix

--- controllers/productController.js
+++ controllers/productController.js
@@ -49,7 +49,6 @@ async function store(req, res) {
     return res.status(200).json(product);
   } catch (err) {
     res.status(400).json(err);
-    
   }
 }
 
@@ -68,8 +67,8 @@ async function update(req, res) {
 
 async function destroy(req, res) {
   try {
-    const product = await Product.findById(req.body.productId);
-    await product.delete();
+    await Product.findByIdAndDelete(req.body.productId);
+
     return res.status(200).send({ message: "Product deleted" });
   } catch (err) {
     return res.status(404).send({ message: "Something went wrong, try again later" });
